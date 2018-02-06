c3ac49408679f25a3d3f77b72621a0279840f714
Aaron Wilder <element1028@gmail.com>
2018-02-05 16:49:44 -0800

    getting first view to work

--- coolbeans/App.js
+++ coolbeans/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.header}>
+          <Text style={styles.heaerText}>Header</Text></View>
+          <Image source={require('./ezgif.com-optimize.gif')} style={styles.img}/>
+          <View style={styles.buttonList}>
+            <TouchableOpacity
+              style={styles.buttons}
+              onPress={()=> console.log('like')}
+              >
+            <Text>Like</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.buttons}
+              onPress={()=> console.log('Share')}
+              >
+            <Text>Share</Text>
+            </TouchableOpacity>
+          
+            <TouchableOpacity
+              style={styles.buttons}
+              onPress={()=> console.log('Comment')}
+              >
+            <Text>Comment</Text>
+            </TouchableOpacity>          
+          </View>
+          {/* <View>
+            <Text>Happy</Text>
+          </View> */}
       </View>
     );
   }
@@ -15,9 +40,48 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'space-between',
+  },
+  header: {
     flex: 1,
-    backgroundColor: '#fff',
+    height: 10,
     alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    width: 'auto',
+    padding: 50,
+    flexDirection: 'row',
+    backgroundColor: 'blue',
   },
+  heaerText: {
+    fontSize: 30,
+    textAlign: 'center',
+    alignSelf: 'center',
+    color: 'white',
+    fontFamily: 'Cochin',
+    fontWeight: 'bold',
+    letterSpacing: 10
+  },
+  img: {
+    width: 320,
+    height: 200
+  },
+  buttonList: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    
+    
+  },
+  buttons: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    margin: 6,
+    height: 40,
+    borderRadius: 4
+  }
 });
